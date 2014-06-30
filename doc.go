9392d2979e421835edde066bc96fb564814c1f1e
Koala Yeung <koalay@gmail.com>
2014-06-30 17:52:37 +0800

    Add proper GPL3 declaration to source files

--- doc.go
+++ doc.go
@@ -1,6 +1,21 @@
-// Copyright 2014 Koala Yeung. All rights reserved.
-// Use of this source code is governed by a GPL v3
-// license that can be found in the LICENSE.md file.
+// Copyright (c) 2014 Yeung Shu Hung (Koala Yeung)
+//
+//  This file is part of RESTit.
+//
+//  RESTit is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  RESTit is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  Use of this source code is governed by the GPL v3 license. A copy
+//  of the licence can be found in the LICENSE.md file along with RESTit.
+//  If not, see <http://www.gnu.org/licenses/>.
+
 
 /*
 	Package RESTit provides helps to those who
--- response.go
+++ response.go
@@ -1,3 +1,21 @@
+// Copyright (c) 2014 Yeung Shu Hung (Koala Yeung)
+//
+//  This file is part of RESTit.
+//
+//  RESTit is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  RESTit is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  Use of this source code is governed by the GPL v3 license. A copy
+//  of the licence can be found in the LICENSE.md file along with RESTit.
+//  If not, see <http://www.gnu.org/licenses/>.
+
 package restit
 
 // Response needed to fulfill this interface
