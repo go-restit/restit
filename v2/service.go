5ad973b556d1f32698a4e2cb9644530778730dca
Koala Yeung <koalay@gmail.com>
2015-12-04 18:28:18 +0800

    Define Service and its helper
    Add Service type to describe a RESTful HTTP service.
    
    Also some helper functions (`NewHTTPService` and `NewHTTPTestService`)
    to create Service with.

---
+++ v2/service.go
@@ -0,0 +1,123 @@
+package restit
+
+import (
+	"net/http"
+	"net/http/httptest"
+)
+
+// HTTPHandler implements CaseHandlerFunc
+func HTTPHandler(req *http.Request) (resp Response, err error) {
+	c := &http.Client{}
+	rawResp, err := c.Do(req)
+	if err != nil {
+		return
+	}
+
+	resp = HTTPResponse{rawResp}
+	return
+}
+
+// NewHTTPService create a normal HTTP service to a real
+// HTTP server
+func NewHTTPService(paths Paths) *Service {
+	return &Service{
+		Paths:   paths,
+		Handler: CaseHandlerFunc(HTTPHandler),
+	}
+}
+
+// HTTPTestHandler implements CaseHandlerFunc
+func HTTPTestHandler(handler http.Handler) func(*http.Request) (Response, error) {
+	return func(req *http.Request) (resp Response, err error) {
+		w := httptest.NewRecorder()
+		handler.ServeHTTP(w, req)
+		resp = HTTPTestResponse{w}
+		return
+	}
+}
+
+// NewHTTPTestService create a dummy service based on httptest.Recorder
+// as request handler
+func NewHTTPTestService(paths Paths, handler http.Handler) *Service {
+	return &Service{
+		Paths:   paths,
+		Handler: CaseHandlerFunc(HTTPTestHandler(handler)),
+	}
+}
+
+// Service provides method to interact with a RESTful service
+// based on the given Paths value
+type Service struct {
+	Paths   Paths
+	Handler CaseHandler
+}
+
+// List sends a GET request
+// to plural path and examine the result
+func (s Service) List(v ...string) *Case {
+	req, err := NewRequest("GET", s.Paths.Plural(v...), nil)
+	if err != nil {
+		panic(err)
+	}
+
+	return &Case{
+		Request: req,
+		Handler: s.Handler,
+	}
+}
+
+// Create sends a POST request (with JSON encoded payload)
+// to plural path and examine the result
+func (s Service) Create(payload interface{}, v ...string) *Case {
+	req, err := NewRequest("GET", s.Paths.Plural(v...), payload)
+	if err != nil {
+		panic(err)
+	}
+
+	return &Case{
+		Request: req,
+		Handler: s.Handler,
+	}
+}
+
+// Update sends a PUT request (wtih JSON encoded payload)
+// to singular path and examine the result
+func (s Service) Update(payload interface{}, v ...string) *Case {
+	req, err := NewRequest("PUT", s.Paths.Singular(v...), payload)
+	if err != nil {
+		panic(err)
+	}
+
+	return &Case{
+		Request: req,
+		Handler: s.Handler,
+	}
+}
+
+// Retrieve sends a GET request
+// to singular path and examine the result
+func (s Service) Retrieve(payload interface{}, v ...string) *Case {
+	req, err := NewRequest("GET", s.Paths.Singular(v...), payload)
+	if err != nil {
+		panic(err)
+	}
+
+	return &Case{
+		Request: req,
+		Handler: s.Handler,
+	}
+}
+
+// Delete sends a DELETE request
+// to singular path and examine the result
+func (s Service) Delete(v ...string) *Case {
+	req, err := NewRequest("DELETE", s.Paths.Singular(v...), nil)
+	if err != nil {
+		panic(err)
+	}
+
+	return &Case{
+		Request: req,
+		Handler: s.Handler,
+	}
+}
---
+++ v2/service_test.go
@@ -0,0 +1,183 @@
+package restit_test
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	restit "github.com/yookoala/restit/v2"
+)
+
+// dummyTestSuite to test dummyServiceHandler below
+func dummyTestSuite(service *restit.Service, paths restit.Paths) (err error) {
+
+	type post struct {
+		ID      string    `json:"id"`
+		Name    string    `json:"name"`
+		Created time.Time `json:"created"`
+		Updated time.Time `json:"updated"`
+	}
+
+	err = func() (err error) {
+		randPath := []string{RandString(20), RandString(20)}
+		testCase := service.List(randPath...)
+		if req := testCase.Request; req == nil {
+			err = fmt.Errorf("[list][request] empty")
+		} else if want, have := paths.Plural(randPath...), req.URL.String(); want != have {
+			err = fmt.Errorf("[list][request.URL] expected %#v, got %#v", want, have)
+		} else if want, have := "GET", req.Method; want != have {
+			err = fmt.Errorf("[list][request.Method] expected %#v, got %#v", want, have)
+		}
+		return
+	}()
+	if err != nil {
+		return
+	}
+
+	err = func() (err error) {
+		testCase := service.Create(post{ID: RandString(20), Name: RandString(20)})
+		if req := testCase.Request; req == nil {
+			err = fmt.Errorf("[create][request] empty")
+		} else if want, have := paths.Plural(), req.URL.String(); want != have {
+			err = fmt.Errorf("[create][request.URL] expected %#v, got %#v", want, have)
+		} else if want, have := "GET", req.Method; want != have {
+			err = fmt.Errorf("[create][request.Method] expected %#v, got %#v", want, have)
+		}
+		return
+	}()
+	if err != nil {
+		return
+	}
+
+	err = func() (err error) {
+		testCase := service.Create(post{ID: RandString(20), Name: RandString(20)}, "foo", "bar")
+		if req := testCase.Request; req == nil {
+			err = fmt.Errorf("[create][request] empty")
+		} else if want, have := paths.Plural("foo", "bar"), req.URL.String(); want != have {
+			err = fmt.Errorf("[create][request.URL] expected %#v, got %#v", want, have)
+		} else if want, have := "GET", req.Method; want != have {
+			err = fmt.Errorf("[create][request.Method] expected %#v, got %#v", want, have)
+		}
+		return
+	}()
+	if err != nil {
+		return
+	}
+
+	err = func() (err error) {
+		postID := RandString(20)
+		testCase := service.Update(post{ID: postID, Name: RandString(20)}, postID)
+		if req := testCase.Request; req == nil {
+			err = fmt.Errorf("[update][request] empty")
+		} else if want, have := paths.Singular(postID), req.URL.String(); want != have {
+			err = fmt.Errorf("[update][request.URL] expected %#v, got %#v", want, have)
+		} else if want, have := "PUT", req.Method; want != have {
+			err = fmt.Errorf("[update][request.Method] expected %#v, got %#v", want, have)
+		}
+		return
+	}()
+	if err != nil {
+		return
+	}
+
+	err = func() (err error) {
+		postID := RandString(20)
+		testCase := service.Retrieve(post{ID: postID, Name: RandString(20)}, postID)
+		if req := testCase.Request; req == nil {
+			err = fmt.Errorf("[retrieve][request] empty")
+		} else if want, have := paths.Singular(postID), req.URL.String(); want != have {
+			err = fmt.Errorf("[retrieve][request.URL] expected %#v, got %#v", want, have)
+		} else if want, have := "GET", req.Method; want != have {
+			err = fmt.Errorf("[retrieve][request.Method] expected %#v, got %#v", want, have)
+		}
+		return
+	}()
+	if err != nil {
+		return
+	}
+
+	err = func() (err error) {
+		postID := RandString(20)
+		testCase := service.Delete(postID)
+		if req := testCase.Request; req == nil {
+			err = fmt.Errorf("[delete][request] empty")
+		} else if want, have := paths.Singular(postID), req.URL.String(); want != have {
+			err = fmt.Errorf("[delete][request.URL] expected %#v, got %#v", want, have)
+		} else if want, have := "DELETE", req.Method; want != have {
+			err = fmt.Errorf("[delete][request.Method] expected %#v, got %#v", want, have)
+		}
+		return
+	}()
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+// dummyServiceHandler provides a dummy service to test with
+func dummyServiceHandler() http.Handler {
+	var handler http.HandlerFunc = func(w http.ResponseWriter, r *http.Request) {
+	}
+	return handler
+}
+
+func TestService(t *testing.T) {
+	noun := restit.NewNoun("post", "posts")
+	paths, err := restit.NewPaths("/dummyAPI", noun)
+	if err != nil {
+		t.Errorf("unexpected error: %#v", err.Error())
+	}
+
+	service := &restit.Service{
+		Paths: paths,
+	}
+
+	// run test suite against the dummy service
+	if err := dummyTestSuite(service, paths); err != nil {
+		t.Errorf("failed running test suite: %s", err.Error())
+	}
+
+}
+
+func TestHTTPService(t *testing.T) {
+
+	// create a real HTTP server with httptest.Server
+	handler := dummyServiceHandler()
+	testServer := httptest.NewServer(handler)
+	defer testServer.Close()
+
+	noun := restit.NewNoun("post", "posts")
+	paths, err := restit.NewPaths(testServer.URL+"/dummyAPI", noun)
+	if err != nil {
+		t.Errorf("unexpected error: %#v", err.Error())
+	}
+
+	// create HTTPService
+	service := restit.NewHTTPService(paths)
+
+	// run test suite against the dummy service
+	if err := dummyTestSuite(service, paths); err != nil {
+		t.Errorf("failed running test suite: %s", err.Error())
+	}
+}
+
+func TestHTTPTestService(t *testing.T) {
+	handler := dummyServiceHandler()
+
+	noun := restit.NewNoun("post", "posts")
+	paths, err := restit.NewPaths("/dummyAPI", noun)
+	if err != nil {
+		t.Errorf("unexpected error: %#v", err.Error())
+	}
+
+	// create HTTPTestService
+	service := restit.NewHTTPTestService(paths, handler)
+
+	// run test suite against the dummy service
+	if err := dummyTestSuite(service, paths); err != nil {
+		t.Errorf("failed running test suite: %s", err.Error())
+	}
+}
